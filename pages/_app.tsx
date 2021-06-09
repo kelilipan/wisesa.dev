3eaa78f01918e73e84ff8f6b9f2e0903e6110718
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-06-09 17:48:14 +0700

    style: increase nprogress zindex and remove spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import "nprogress/nprogress.css";
 import NProgress from "nprogress";
 import Router from "next/router";
 
+NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
