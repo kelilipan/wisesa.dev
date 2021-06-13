f3f29deb4c3fb32fee379fd309db9c2e36b5f7f6
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-06-13 15:00:39 +0700

    feat(pwa): add support for next/image

--- next.config.js
+++ next.config.js
@@ -26,5 +26,115 @@ module.exports = withPWA({
     sourcemap: false,
     dest: "public",
     register: true,
+    // https://github.com/shadowwalker/next-pwa/issues/147
+    publicExcludes: ["!blog/**/*"],
+    runtimeCaching: [
+      {
+        // MUST be the same as "start_url" in manifest.json
+        urlPattern: "/",
+        // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
+        // use StaleWhileRevalidate if you want to prompt user to reload when new version available
+        handler: "NetworkFirst",
+        options: {
+          // don't change cache name
+          cacheName: "start-url",
+          expiration: {
+            maxEntries: 1,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts",
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-image-assets",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-js-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-style-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-data-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\/api\/.*$/i,
+        handler: "NetworkFirst",
+        method: "GET",
+        options: {
+          cacheName: "apis",
+          expiration: {
+            maxEntries: 16,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+        },
+      },
+      {
+        urlPattern: /\/_next\/image\?url=.*$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-image",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /.*/i,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "others",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10,
+        },
+      },
+    ],
   },
 });
