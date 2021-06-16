9fa2fcfb9127dbf8ead706dac53dda537476d2f2
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-06-16 23:19:20 +0700

    chore: preconnect google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,9 @@ export default class Document extends NextDocument {
     return (
       <Html lang="id">
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter"
             rel="stylesheet"
           />
           <script async data-api="/_hive" src="/bee.js"></script>
