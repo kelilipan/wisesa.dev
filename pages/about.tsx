ce17c55fac4cc2a04a0f0f82d3d110227e914353
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-06-19 17:59:16 +0700

    style: add zindex

--- pages/about.tsx
+++ pages/about.tsx
@@ -75,146 +75,148 @@ const about = () => {
           <Doodle1 />
         </Box>
       </Flex>
-      <Text pt="4">
-        Hello, my name is <b>Anvaqta Tangguh Wisesa</b>, you can call me{" "}
-        <b>Wisesa</b> or <b>Esa</b> for short. Yes, i changed my nickname haha,
-        if you're my school/college friend you maybe know me as <b>Vaq</b> or{" "}
-        <b>Anvaq</b> but that's ok, you can call me anything you want as long as
-        it's my name and not someone else <i>wkw</i>.
-      </Text>
-      <Text>
-        I grew up in a small village located in{" "}
-        <Link
-          textDecoration="underline"
-          isExternal
-          href="https://www.google.com/search?q=grobogan+jawa+tengah"
-        >
-          Grobogan, Jawa Tengah
-        </Link>
-        . I'm currently pursuing my Bachelor's degree in Informatics (Computer
-        science) at{" "}
-        <Link
-          textDecoration="underline"
-          isExternal
-          href="https://telkomuniversity.ac.id/"
-        >
-          Telkom University
-        </Link>{" "}
-        also working as a Software engineer at a start-up called{" "}
-        <Link
-          textDecoration="underline"
-          href="https://ketringan.com"
-          isExternal
-        >
-          Ketringan Indonesia
-        </Link>
-        .
-      </Text>
-      <Text>
-        I love exploring tech related stuff. When i was in college i join many
-        community and laboratory to expand my knowledge, I'm member of{" "}
-        <Link
-          textDecoration="underline"
-          href="https://www.instagram.com/cciunitel/?hl=en"
-          isExternal
-        >
-          CCI-Unitel
-        </Link>
-        , part of{" "}
-        <Link
-          textDecoration="underline"
-          href="https://www.instagram.com/pramukatelu/?hl=en"
-          isExternal
-        >
-          Pramuka Tel-U
-        </Link>
-        , and i'm the (ex) lab.assistant of{" "}
-        <Link
-          textDecoration="underline"
-          href="http://ailabtelkom.github.io/"
-          isExternal
-        >
-          Artificial Intelligence TelU
-        </Link>
-        . Also, I joined many competition such as Competitive programming, UI/UX
-        Design, and{" "}
-        <Link
-          textDecoration="underline"
-          href="https://youngster.id/news/ketringan-com-juara-di-brixgoogle-cloud-hackathon/"
-        >
-          Hackathon
-        </Link>
-        .
-      </Text>
-
-      <Text mt="2">
-        Here are several tools that I used (and currently learning how to use
-        it):
-      </Text>
-      <SimpleGrid columns={[2, 4]} pt="2">
-        <Box>
-          <Heading as="h5" fontSize="xl">
-            Languages
-          </Heading>
-          {languages.map((lang, idx) => (
-            <Text key={idx}>{lang}</Text>
-          ))}
-        </Box>
-        <Box>
-          <Heading as="h5" fontSize="xl">
-            Tools
-          </Heading>
-          {tools.map((tool, idx) => (
-            <Text key={idx}>{tool}</Text>
-          ))}
-        </Box>
-        <Box>
-          <Heading as="h5" fontSize="xl">
-            Frameworks
-          </Heading>
-          {frameworks.map((framework, idx) => (
-            <Text key={idx}>{framework}</Text>
-          ))}
-        </Box>
-        <Box>
-          <Heading as="h5" fontSize="xl">
-            Currently Learning
-          </Heading>
-          {learnings.map((learning, idx) => (
-            <Text key={idx}>{learning}</Text>
-          ))}
-        </Box>
-      </SimpleGrid>
-      <Box pt="4">
-        <Text>
-          I’m always excited to connect with everyone so please don’t hesitate
-          to{" "}
-          <Link textDecoration="underline" href="mailto:hi@wisesa.dev">
-            get in touch
-          </Link>{" "}
-          with me by following my social media bellow:
+      <Box zIndex="5">
+        <Text pt="4">
+          Hello, my name is <b>Anvaqta Tangguh Wisesa</b>, you can call me{" "}
+          <b>Wisesa</b> or <b>Esa</b> for short. Yes, i changed my nickname
+          haha, if you're my school/college friend you maybe know me as{" "}
+          <b>Vaq</b> or <b>Anvaq</b> but that's ok, you can call me anything you
+          want as long as it's my name and not someone else <i>wkw</i>.
         </Text>
-        <UnorderedList>
-          {socials.map((data, idx) => (
-            <ListItem key={idx}>
-              {data.name} -{" "}
-              <Link textDecoration="underline" href={data.url} isExternal>
-                {data.url}
-              </Link>
-            </ListItem>
-          ))}
-        </UnorderedList>
-        <Text mt="2">
-          Also, you can{" "}
+        <Text>
+          I grew up in a small village located in{" "}
           <Link
             textDecoration="underline"
-            href="http://gg.gg/wisesa-cv"
+            isExternal
+            href="https://www.google.com/search?q=grobogan+jawa+tengah"
+          >
+            Grobogan, Jawa Tengah
+          </Link>
+          . I'm currently pursuing my Bachelor's degree in Informatics (Computer
+          science) at{" "}
+          <Link
+            textDecoration="underline"
+            isExternal
+            href="https://telkomuniversity.ac.id/"
+          >
+            Telkom University
+          </Link>{" "}
+          also working as a Software engineer at a start-up called{" "}
+          <Link
+            textDecoration="underline"
+            href="https://ketringan.com"
             isExternal
           >
-            read my cv here
+            Ketringan Indonesia
           </Link>
-          . Anyway, thanks for visiting my profile :)
+          .
         </Text>
+        <Text>
+          I love exploring tech related stuff. When i was in college i join many
+          community and laboratory to expand my knowledge, I'm member of{" "}
+          <Link
+            textDecoration="underline"
+            href="https://www.instagram.com/cciunitel/?hl=en"
+            isExternal
+          >
+            CCI-Unitel
+          </Link>
+          , part of{" "}
+          <Link
+            textDecoration="underline"
+            href="https://www.instagram.com/pramukatelu/?hl=en"
+            isExternal
+          >
+            Pramuka Tel-U
+          </Link>
+          , and i'm the (ex) lab.assistant of{" "}
+          <Link
+            textDecoration="underline"
+            href="http://ailabtelkom.github.io/"
+            isExternal
+          >
+            Artificial Intelligence TelU
+          </Link>
+          . Also, I joined many competition such as Competitive programming,
+          UI/UX Design, and{" "}
+          <Link
+            textDecoration="underline"
+            href="https://youngster.id/news/ketringan-com-juara-di-brixgoogle-cloud-hackathon/"
+          >
+            Hackathon
+          </Link>
+          .
+        </Text>
+
+        <Text mt="2">
+          Here are several tools that I used (and currently learning how to use
+          it):
+        </Text>
+        <SimpleGrid columns={[2, 4]} pt="2">
+          <Box>
+            <Heading as="h5" fontSize="xl">
+              Languages
+            </Heading>
+            {languages.map((lang, idx) => (
+              <Text key={idx}>{lang}</Text>
+            ))}
+          </Box>
+          <Box>
+            <Heading as="h5" fontSize="xl">
+              Tools
+            </Heading>
+            {tools.map((tool, idx) => (
+              <Text key={idx}>{tool}</Text>
+            ))}
+          </Box>
+          <Box>
+            <Heading as="h5" fontSize="xl">
+              Frameworks
+            </Heading>
+            {frameworks.map((framework, idx) => (
+              <Text key={idx}>{framework}</Text>
+            ))}
+          </Box>
+          <Box>
+            <Heading as="h5" fontSize="xl">
+              Currently Learning
+            </Heading>
+            {learnings.map((learning, idx) => (
+              <Text key={idx}>{learning}</Text>
+            ))}
+          </Box>
+        </SimpleGrid>
+        <Box pt="4">
+          <Text>
+            I’m always excited to connect with everyone so please don’t hesitate
+            to{" "}
+            <Link textDecoration="underline" href="mailto:hi@wisesa.dev">
+              get in touch
+            </Link>{" "}
+            with me by following my social media bellow:
+          </Text>
+          <UnorderedList>
+            {socials.map((data, idx) => (
+              <ListItem key={idx}>
+                {data.name} -{" "}
+                <Link textDecoration="underline" href={data.url} isExternal>
+                  {data.url}
+                </Link>
+              </ListItem>
+            ))}
+          </UnorderedList>
+          <Text mt="2">
+            Also, you can{" "}
+            <Link
+              textDecoration="underline"
+              href="http://gg.gg/wisesa-cv"
+              isExternal
+            >
+              read my cv here
+            </Link>
+            . Anyway, thanks for visiting my profile :)
+          </Text>
+        </Box>
       </Box>
     </Main>
   );
