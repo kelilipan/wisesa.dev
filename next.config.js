3fee0d053141583d2bdaf7c8087a028a104f3c62
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-03 12:54:21 +0700

    feat: disable i18n routing

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
     domains: ["pbs.twimg.com"],
   },
   // https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ["en-US", "id"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en-US",
-  },
   // https://panelbear.com/docs/integration-nextjs/
   async rewrites() {
     return [
