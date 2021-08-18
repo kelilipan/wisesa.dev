532501e0825c7454c613c737be88aea7ed56d830
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-18 11:42:26 +0700

    chore: upgrade next

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module "mdx-prism";
-declare module "nprogress";
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
