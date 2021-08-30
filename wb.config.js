a3f3415feae22f2c7b9f4500eb6d28898ce24ac6
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-30 12:04:02 +0700

    chore: next-pwa is trully zero config

--- wb.config.js
+++ wb.config.js
@@ -14,29 +14,4 @@ module.exports = {
     "!me-2.svg",
     "!sitemap.xml",
   ],
-  buildExcludes: [/static\/image\/.*$/],
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-image-assets",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:woff|woff2|ttf|otf)$/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "custom-fonts",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    }
-  ],
 };
