3d2e01e9cd90541b3e784a2492afbdea8fcf8b84
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-30 11:34:49 +0700

    feat: custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,22 @@
+import Main from "@/components/Main";
+import { RouteLink } from "@/components/RouteLink";
+import { Box, Button, Text, Image } from "@chakra-ui/react";
+import { FaHome } from "react-icons/fa";
+
+const offline = () => {
+  return (
+    <Main>
+      <Box textAlign="center">
+        <Image width={"280px"} height={"216px"} src="/dino.gif" mx="auto" />
+        <Text mt="4">Uh... something happens :((</Text>
+        <RouteLink href="/">
+          <Button variant="outline" size="sm" mt="2" leftIcon={<FaHome />}>
+            Home
+          </Button>
+        </RouteLink>
+      </Box>
+    </Main>
+  );
+};
+
+export default offline;
