11cae64c90758a3bf04ad96114219640bbdc6be9
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-08-30 12:01:19 +0700

    chore: update wb config

--- wb.config.js
+++ wb.config.js
@@ -17,32 +17,6 @@ module.exports = {
   buildExcludes: [/static\/image\/.*$/],
   runtimeCaching: [
     {
-      // MUST be the same as "start_url" in manifest.json
-      urlPattern: "/",
-      // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
-      // use StaleWhileRevalidate if you want to prompt user to reload when new version available
-      handler: "NetworkFirst",
-      options: {
-        // don't change cache name
-        cacheName: "start-url",
-        expiration: {
-          maxEntries: 1,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    },
-    {
       urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
       handler: "StaleWhileRevalidate",
       options: {
@@ -63,41 +37,6 @@ module.exports = {
           maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
         },
       },
-    },
-    {
-      urlPattern: /\.(?:js)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-js-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:css|less)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-style-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\/api\/.*$/i,
-      handler: "NetworkFirst",
-      method: "GET",
-      options: {
-        cacheName: "apis",
-        expiration: {
-          maxEntries: 16,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-      },
-    },
+    }
   ],
 };
