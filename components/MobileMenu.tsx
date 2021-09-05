20364105c4d4c425368d98855ef332e735e5ffa9
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-09-05 17:57:22 +0700

    feat: add mobile menu

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,111 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import Link from "@/components/Link";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import ThemeSwitcher from "./ThemeSwitcher";
+interface MobileMenuProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+const links = [
+  {
+    text: "Home",
+    url: "/",
+  },
+  {
+    text: "Blog",
+    url: "/blog",
+  },
+  {
+    text: "Projects",
+    url: "/projects",
+  },
+  {
+    text: "Snippets",
+    url: "/snippet",
+  },
+  {
+    text: "Timeline",
+    url: "https://timeline.wisesa.dev",
+  },
+  {
+    text: "About",
+    url: "/about",
+  },
+];
+
+const MobileMenu = ({ isOpen, onClose }: MobileMenuProps) => {
+  const closeModal = () => onClose();
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-50 overflow-y-auto"
+        onClose={closeModal}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 backdrop-blur" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md overflow-hidden text-left align-middle transition-all transform bg-white dark:bg-dark shadow-xl rounded">
+              <div className="mt-2">
+                <div className="flex flex-col w-full">
+                  {links.map((data, idx) => (
+                    <Link
+                      className="w-full text-center font-semibold p-4 hover:bg-gray-100 dark:hover:bg-light"
+                      href={data.url}
+                      isExternal={data.text === "Timeline"}
+                      key={idx}
+                    >
+                      {data.text}{" "}
+                      {data.text === "Timeline" && (
+                        <FaExternalLinkAlt className="text-xs inline ml-1 mb-1" />
+                      )}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center p-4">
+                <ThemeSwitcher />
+                <button
+                  type="button"
+                  className="hover:bg-gray-100 font-semibold dark:hover:bg-light p-4"
+                  onClick={closeModal}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+export default MobileMenu;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "@/components/Link";
 import { FaBars, FaExternalLinkAlt } from "react-icons/fa";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const ThemeSwitcher = dynamic(() => import("./ThemeSwitcher"), { ssr: false });
+const MobileMenu = dynamic(() => import("./MobileMenu"), { ssr: false });
 const Navbar = () => {
   const links = [
     {
@@ -26,45 +28,54 @@ const Navbar = () => {
       url: "/about",
     },
   ];
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <nav className="z-50 border-b-2 border-gray-200 dark:border-light sticky top-0 bg-white dark:bg-dark">
-      <div className="flex max-w-5xl mx-auto items-center">
-        <Link
-          href="/"
-          className="font-doodle font-semibold text-center text-xl py-2 md:py-4 px-2 hover:bg-gray-300 dark:hover:bg-light transition duration-200 ease-in-out"
-        >
-          Wisesa.dev
-        </Link>
-        <div className="hidden md:flex w-full justify-between items-center ">
-          <div className="flex">
-            {links.map((link) => {
-              return (
-                <Link
-                  href={link.url}
-                  key={link.url}
-                  className="font-semibold text-center text-sm min-w-16 py-5 px-2 hover:bg-gray-300 dark:hover:bg-light transition duration-200 ease-in-out"
-                >
-                  {link.text}
-                  {link.text === "Timeline" && (
-                    <FaExternalLinkAlt
-                      className="ml-2 inline"
-                      style={{ fontSize: "10px" }}
-                    />
-                  )}
-                </Link>
-              );
-            })}
+    <>
+      <nav className="z-50 border-b-2 border-gray-200 dark:border-light sticky top-0 bg-white dark:bg-dark">
+        <div className="flex max-w-5xl mx-auto items-center">
+          <Link
+            href="/"
+            className="font-doodle font-semibold text-center text-xl py-2 md:py-4 px-2 hover:bg-gray-300 dark:hover:bg-light transition duration-200 ease-in-out"
+          >
+            Wisesa.dev
+          </Link>
+          <div className="hidden md:flex w-full justify-between items-center ">
+            <div className="flex">
+              {links.map((link) => {
+                return (
+                  <Link
+                    href={link.url}
+                    key={link.url}
+                    className="font-semibold text-center text-sm min-w-16 py-5 px-2 hover:bg-gray-300 dark:hover:bg-light transition duration-200 ease-in-out"
+                  >
+                    {link.text}
+                    {link.text === "Timeline" && (
+                      <FaExternalLinkAlt
+                        className="ml-2 inline"
+                        style={{ fontSize: "10px" }}
+                      />
+                    )}
+                  </Link>
+                );
+              })}
+            </div>
+            <ThemeSwitcher />
           </div>
-          <ThemeSwitcher />
+          <div className="flex ml-auto md:hidden">
+            <button
+              onClick={() => setShowMenu(true)}
+              aria-labelledby="open navigation menu"
+              className="h-[44px] px-3 hover:bg-gray-300 dark:hover:bg-light transition duration-200 ease-in-out"
+            >
+              <FaBars />
+            </button>
+          </div>
         </div>
-        <div className="flex ml-auto md:hidden">
-          <button className="h-[44px] px-3 hover:bg-gray-300 transition duration-200 ease-in-out">
-            <FaBars />
-          </button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+
+      <MobileMenu isOpen={showMenu} onClose={() => setShowMenu(false)} />
+    </>
   );
 };
 
