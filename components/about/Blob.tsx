f2c3bc5294eb36ea0cd188682f28f4a51583e722
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-09-14 17:09:44 +0700

    feat: using picture

--- components/about/Blob.tsx
+++ components/about/Blob.tsx
@@ -11,15 +11,16 @@ const Blob = () => {
   return (
     <svg viewBox="0 0 200 200" className="w-[400px] h-[400px]">
       <defs>
-        <linearGradient id="gradient" gradientTransform="rotate(120)">
-          <stop id="gradientStop1" offset="0%" stopColor="var(--startColor)" />
-          <stop id="gradientStop2" offset="100%" stopColor="var(--stopColor)" />
-        </linearGradient>
-        {/* <pattern id="me" patternUnits="userSpaceOnUse" width="300" height="300">
+        <pattern
+          id="me-picture"
+          patternUnits="userSpaceOnUse"
+          width="300"
+          height="300"
+        >
           <image href="/me-2.jpg" width="200" height="200" x="0" y="0" />
-        </pattern> */}
+        </pattern>
       </defs>
-      <path d="" id="mask-blob" fill="url('#gradient')"></path>
+      <path d="" id="mask-blob" fill="url('#me-picture')"></path>
     </svg>
   );
 };
