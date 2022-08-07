02a0b72b4b6f578409508555bfee425968b37594
Wisesa <anvaqta.wisesa@tokopedia.com>
2022-08-07 18:25:44 +0700

    feat: reduce enter animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -76,7 +76,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
             initial="initial"
             variants={{
               initial: { opacity: 0, y: 5 },
-              enter: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+              enter: { opacity: 1, y: 0, transition: { duration: 0.1 } },
               exit: { opacity: 0, transition: { duration: 0.1 } },
             }}
           >
